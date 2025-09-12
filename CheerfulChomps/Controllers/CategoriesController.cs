6b4c579053f2d077ced8f8431b966c54d57b0f1f
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-09-12 15:54:05 -0400

    Add project files.

---
+++ CheerfulChomps/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using CheerfulChomps.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheerfulChomps.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            // create mock list of categories in memory
+            var categories = new List<Category>();
+
+            for (var i = 1; i < 16; i++)
+            {
+                categories.Add(new Category { CategoryId = i, Name = "Category " + i.ToString() });
+            }
+
+            // pass list to view for display
+            return View(categories);
+        }
+    }
+}
