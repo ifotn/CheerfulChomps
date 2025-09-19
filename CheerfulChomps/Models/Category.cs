ea3f8464ccce9e2a38723b456bcc5145e611f30b
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-09-19 16:35:32 -0400

    l3/3 - product model + model relationships

--- CheerfulChomps/Models/Category.cs
+++ CheerfulChomps/Models/Category.cs
@@ -4,5 +4,9 @@
     {
         public int CategoryId { get; set; }
         public string Name { get; set; }
+
+        // child ref to list of products: 1 Category / Many Products
+        // must be nullable ? so we can first create a Category without Products, then attach Products after
+        public List<Product>? Products { get; set; }
     }
 }
---
+++ CheerfulChomps/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace CheerfulChomps.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string? Image { get; set; }
+
+        // FK: every Product belongs to 1 Category
+        public int CategoryId { get; set; }
+
+        // Parent ref to Category that the Product belongs to
+        public Category Category { get; set; }
+    }
+}
